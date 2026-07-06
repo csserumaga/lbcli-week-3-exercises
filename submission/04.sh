69aaafd9f7cf6817f889524180a6289126fa70db
csserumaga <alltechissues@gmail.com>
2026-07-06 21:15:33 +0300

    added fourth file

--- submission/04.sh
+++ submission/04.sh
@@ -1,5 +1,8 @@
 # Create a 2-of-3 P2SH multisig address from the public keys below:
 
-# 024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d
-# 035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617
-# 03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66
+key1="024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d"
+key2="035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617"
+key3="03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66"
+
+addr=$(bitcoin-cli -regtest createmultisig 2 "[\"$key1\",\"$key2\",\"$key3\"]" legacy | jq -r '.address')
+echo "$addr"
